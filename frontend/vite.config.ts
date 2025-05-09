a9c6bb27503693adbaa51ce06067a6b0578f9da5
Carlos Vitor Cardoso da Silva <cvcardos5@gmail.com>
2025-05-09 09:51:04 -0300

    create frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/files': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
